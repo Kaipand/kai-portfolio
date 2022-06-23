8783e434cba5f4093fd0dbddb81a38a941830f67
Kai Pandit <kaipandit1@gmail.com>
2022-06-23 21:01:54 +0000

    Weeks 1 and 2, and the start of week 3. Added a flashlight in script.js updated

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,13 +6,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList; // import the ArrayList class
+
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
+
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.setContentType("application/json");
+    ArrayList<String> list = new ArrayList<String> ();
+
+    list.add("hi");
+    list.add("my");
+    list.add("name");
+    list.add("is");
+    list.add("kai");
+    list.add("pandit");
+    String json = new Gson().toJson(list);
+    response.getWriter().println(json);
   }
 }
